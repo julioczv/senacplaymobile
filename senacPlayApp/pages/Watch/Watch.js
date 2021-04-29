78618ef0b5fbf263c8009ac17d199a8a0900f528
Jukita <zjukiita@gmail.com>
2021-04-29 14:38:53 -0300

    Arrumei A Cagada

--- senacPlayApp/pages/Watch/Watch.js
+++ senacPlayApp/pages/Watch/Watch.js
@@ -1,15 +1,21 @@
+import Video from 'react-native-video'
 import { View, Text } from 'react-native'
 import React from 'react'
+import styles from './styles'
+import { useNavigation } from '@react-navigation/native';
 
 
-const Watch = () => {
+
+const Watch = ({ item }) => {
     return (
         <View>
-            
+            <View style={styles.simsim}>
+                <Text> {item?.sinopse}</Text>
+            </View>
         </View>
     )
-}
+};
 
 
 
-export default Watch ; 
+export default Watch;
